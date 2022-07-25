112af8b394c4fa9034c5ca5282cf40f185470b66
anushk777 <anushk15tiwari@gmail.com>
2022-07-26 00:39:22 +0530

    NumbersAPI
    An Awesome Website that tells about amazing facts about Numbers.

---
+++ index.js
@@ -0,0 +1,38 @@
+let btn = document.getElementById('btnn')
+let crd = document.getElementById('crd')
+
+btn.addEventListener('click', getFacts)
+
+function getFacts() {
+  let input = document.getElementById('input-val').value
+  let crd_title = document.getElementById('crd-title')
+  let crd_txt = document.getElementById('crd-txt')
+  // console.log(input);
+  if (input != '') {
+    crd.classList.remove('d-none')
+    fetch(`http://numbersapi.com/${input}`)
+      .then((response) => response.text())
+      .then((data) => {
+        crd_title.innerHTML = input
+        crd_txt.innerHTML = data
+      })
+      .catch((err) => console.error(err))
+  }
+}
+
+
+
+let cpy = document.getElementById('cpy-btn')
+cpy.addEventListener('click', cpyTxt)
+
+function cpyTxt() {
+  const txtArea = document.createElement('textarea')
+  let crd_txt = document.getElementById('crd-txt').innerHTML
+
+  txtArea.value = crd_txt
+  document.body.append(txtArea)
+  txtArea.select()
+  document.execCommand('copy')
+  txtArea.remove()
+  alert('FACt has been copied successfully.')
+}
